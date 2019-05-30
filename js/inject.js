d46459fbd2ce3e09689493e016e9ed5387a4f4e2
Andres Vargas <zodman@gmail.com>
2019-05-30 12:30:39 -0500

    fix last ux

--- js/inject.js
+++ js/inject.js
@@ -1,8 +1,11 @@
 var url_path="https://www.torrentleech.org/rss/download/";
 
+setTimeout(function () {
+
 chrome.storage.local.get("torrentleech.key", function(obj_key){
     var key = obj_key["torrentleech.key"];
-    var a_links = $(".quickdownload a");
+    var a_links = $(".td-quick-download a.download");
+    
     $.each(a_links, function(index, a_link){
         var url = $(a_link).attr("href");
         var url_splited = url.split("/");
@@ -12,9 +15,11 @@ chrome.storage.local.get("torrentleech.key", function(obj_key){
         var new_href=url_path +  download_id + "/" + key + "/" + filename; 
         var new_a = $("<a>").attr("href", new_href).text("DD");
         $(a_link).after(new_a);
-        //console.log("info", filename, download_id);
+//        console.log("info", filename, download_id);
     });
 
 
-    console.log("finish Inject")
+ //   console.log("finish Inject", a_links)
 });
+
+}, 1000);
